113868342c4a6e2935da109ee2185140404bef2b
douchen <douchen@pwrd.com>
2023-11-23 14:45:48 +0800

    完成

--- main.js
+++ main.js
@@ -1,49 +1,10 @@
-const zy = require('./data/zy.js')
 const ExcelJS = require('exceljs');
-
 const puppeteer = require('puppeteer');
-// 专业我们已经有了，做为纵轴，我们仍然需要一个横轴
+
+const zy = require('./data/zy.js') // zy列表，如果过长建议分切分为多个同时进行，之后再进行合并
+const SSDM = 11 // 地区：北京
+
 let schList = []
-const arr = []
-const startBroser =async ()=>{
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    page.setRequestInterception(true)
-    
-    page.on('request', logRequest)
-    page.on('response', async (response) => {
-        if(response.url() === 'https://yz.chsi.com.cn/zsml/queryAction.do'){
-            console.log('收到返回', await response.text())
-        }
-    })
-    await page.goto('https://yz.chsi.com.cn/zsml/queryAction.do');
-    await page.pdf({path: 'hn.pdf', format: 'A4'});
-    // 到页面里面了，找到当前页面的所有学校
-    const schoolList = await page.$evaluate('#ssdm', el => {
-        return el.options.map(item => {
-            return {
-                name: item.text,
-                value: item.value
-            }
-        })
-    })
-    await page.close()
-}
-function logRequest(interceptedRequest) {
-    if(interceptedRequest.url() && interceptedRequest.url() === 'https://yz.chsi.com.cn/zsml/queryAction.do'){
-        // 如果是请求学校列表的请求，那么我们就转发
-        console.log('请求重写', interceptedRequest.url());
-        return  interceptedRequest.continue({
-            postData:'ssdm=11&dwmc=&mldm=zyxw&mlmc=&yjxkdm=0252&zymc=&xxfs=',
-            method: 'POST',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                ...interceptedRequest.headers()
-            }
-        });
-    }
-    return  interceptedRequest.continue();
-}
 const getAllSchoolList = async ()=>{
     const workbook = new ExcelJS.Workbook();
     const sheet = workbook.addWorksheet('test');
@@ -52,7 +13,7 @@ const getAllSchoolList = async ()=>{
    
     // 到页面里面了，找到当前页面的所有学校
     let ret = []
-    for(let i = 0; i < 8; i++){  // 8是页数
+    for(let i = 0; i < 8; i++){  // 8是北京的学校页数
         ret = [...ret, ...await handleGetSchool(page, i*20)]
     }
     schList = ret
@@ -74,7 +35,7 @@ const getAllSchoolList = async ()=>{
         for (let index = 0; index < zy.length; index++) {
             const zyItem = zy[index];
             const zyBroser =await puppeteer.launch({headless: "new"});
-            console.log('新建浏览器 for ', zyItem)
+            console.log('launch broswer for ', zyItem)
             const rowValues = {
                 0: zyItem.mc,
                 1: zyItem.mc,
@@ -91,29 +52,10 @@ const getAllSchoolList = async ()=>{
                 }));
             }
         }
-        // return Promise.all(zy.map(async (zyItem) => {
-        //     const zyBroser =await puppeteer.launch();
-        //     console.log('新建浏览器 for ', zyItem)
-        //     const rowValues = {
-        //         0: zyItem.mc,
-        //         1: zyItem.mc,
-        //         2: zyItem.dm,
-        //     }
-        //     await mapSchoolPages();
-        //     console.log(rowValues);
-        //     sheet.addRow(rowValues).commit();
-        //     zyBroser.close()
-        //     async function mapSchoolPages() {
-        //         return Promise.all(schList.map(async (item, index) => {
-        //             let length = await handleGetZy(zyBroser, zyItem.dm, item);
-        //             rowValues[index+3] = length;
-        //         }));
-        //     }
-        // }))
     }
 }
 const handleGetSchool = async(page, start)=>{
-    await page.goto(`https://yz.chsi.com.cn/sch/search.do?ssdm=11&start=${start}`);
+    await page.goto(`https://yz.chsi.com.cn/sch/search.do?ssdm=${SSDM}&start=${start}`);
     const item =await page.$('.sch-list-container');
     const elent = item.$$eval('.name.js-yxk-yxmc',  nodes => nodes.map(n => {
         return n.innerText
@@ -122,7 +64,7 @@ const handleGetSchool = async(page, start)=>{
 }
 const handleGetZy = async(broser, zy, sch)=>{
     const page = await broser.newPage();
-    await page.goto(`https://yz.chsi.com.cn/zsml/querySchAction.do?ssdm=11&dwmc=${sch}&yjxkdm=${zy}`, {timeout: 0})
+    await page.goto(`https://yz.chsi.com.cn/zsml/querySchAction.do?ssdm=${SSDM}&dwmc=${sch}&yjxkdm=${zy}`, {timeout: 0})
     // await page.pdf({path: `./results/pics/${sch}-${zy}.pdf`, format: 'A4'}); // 不再打印
     const item = await page.$('.zsml-zy-filter')
     if(item === null ){
